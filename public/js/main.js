5dae94fd4895fef1021e1f1522c90a628c050280
Maxence <lemairemaxence@hotmail.com>
2021-01-22 16:57:16 +0100

    en cours

---
+++ public/js/main.js
@@ -0,0 +1,44 @@
+// PATIENTS
+
+class Patient {
+    constructor (nom, maladie, argent, poche, etat, seDeplacer, prendreMedoc, payer) {
+        this.nom = nom
+        this.maladie = maladie
+        this.argent = argent
+        this.poche = poche
+        this.etat = etat
+        seDeplacer()
+        prendreMedoc()
+        payer()
+    }
+}
+
+let Marcus = new Patient ("Marcus", "mal indenté", 100, "vide", "malade")
+let Optimus = new Patient ("Optimus", "unsave", 200, "vide", "malade")
+let Sangoku = new Patient ("Sangoku", 404, 80, "vide", "malade")
+let DarthVader = new Patient ("DarthVader", "azmatique", 110, "vide", "malade")
+let Semicolon = new Patient ("Semicolon", "synthaxError", 60, "vide", "malade")
+
+// DOCTOR
+
+let doctor = {
+    salleAttente : [],
+    cabinet : {
+        room : [],
+        chat : {
+            nom : "chat",
+            race : "sphynx",
+        }
+    },
+}
+
+// etat du patien change en "traitement" quand il va chez le doc
+// argent du patient diminue de 50 euros
+
+// PHARMACIE
+
+let pharmacie = {
+
+}
+
+let cimetière = []
